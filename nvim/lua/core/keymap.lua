b07c6eed7b7a0cd0f608561df0b02449f3a97e9d
fugwenna <fugwenna@gmail.com>
2024-05-28 08:49:40 -0400

    wip: moving some git things around

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>h", vim.cmd.split)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
