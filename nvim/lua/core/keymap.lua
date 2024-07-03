103243e49c766fc865735d309858d27a0b6cca56
fugwenna <fugwenna@gmail.com>
2024-07-03 19:38:22 -0400

    chore: updates

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -2,4 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>h", vim.cmd.split)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>E", vim.diagnostic.setqflist)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.setqflist)
+--vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, {})
