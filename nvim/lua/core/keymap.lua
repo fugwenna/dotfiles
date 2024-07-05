745316533059d87cec7e5e1587445ea65f6ebf0f
fugwenna <fugwenna@gmail.com>
2024-07-05 10:47:46 -0400

    fix: corrected keybinds

--- nvim/lua/core/keymap.lua
+++ nvim/lua/core/keymap.lua
@@ -2,5 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>h", vim.cmd.split)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-vim.keymap.set("n", "<leader>e", vim.diagnostic.setqflist)
---vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, {})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
